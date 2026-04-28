a84e22a6ebfbf4d6e7917cc9c5e2aaa198f9346e
professor-aldenorneto <aldenor.neteo@educa.go.gov.br>
2026-04-28 11:29:03 -0300

    Update Program.cs

--- projeto-02/Program.cs
+++ projeto-02/Program.cs
@@ -1,1 +1,39 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exercicio_classe3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Pessoa p1 = new Pessoa();
+            Pessoa p2 = new Pessoa();
+
+            Console.WriteLine("Dados da primeira pessoa:");
+            Console.Write("Nome: ");
+            p1.nome = Console.ReadLine();
+            Console.Write("Idade: ");
+            p1.idade = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Dados da segunda pessoa:");
+            Console.Write("Nome: ");
+            p2.nome = Console.ReadLine();
+            Console.Write("Idade: ");
+            p2.idade = int.Parse(Console.ReadLine());
+
+            if (p1.idade > p2.idade)
+            {
+                Console.WriteLine("Pessoa mais velha: "+p1.nome);
+            }
+            else
+            {
+                Console.WriteLine("Pessoa mais velha: "+p2.nome);
+            }
+        }
+    }
+}
 
