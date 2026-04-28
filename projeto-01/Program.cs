4961c52d8cf0ba75a451884b3c43cba2559947bb
professor-aldenorneto <aldenor.neteo@educa.go.gov.br>
2026-04-28 10:54:33 -0300

    Update Program.cs

--- projeto-01/Program.cs
+++ projeto-01/Program.cs
@@ -1,1 +1,34 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
+namespace exercicio_classe2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Funcionario f1 = new Funcionario();
+            Funcionario f2 = new Funcionario();
+            
+            Console.WriteLine("Dados do primeiro funcionário:");
+            Console.Write("Nome: ");
+            f1.nome=Console.ReadLine();
+            Console.Write("Salário: ");
+            f1.salario = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Dados do segundo funcionário:");
+            Console.Write("Nome: ");
+            f2.nome = Console.ReadLine();
+            Console.Write("Salário: ");
+            f2.salario = double.Parse(Console.ReadLine());
+
+            double salario_media = (f1.salario + f2.salario) / 2.0;
+            Console.WriteLine("Salário médio = " +salario_media);
+           
+
+        }
+    }
+}
