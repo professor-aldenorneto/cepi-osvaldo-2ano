147c233d43ea8ed230dc2ea7d8bb021e73b740da
professor-aldenorneto <aldenor.neteo@educa.go.gov.br>
2026-04-28 11:29:23 -0300

    Add files via upload

---
+++ projeto-02/Pessoa.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exercicio_classe3
+{
+    internal class Pessoa
+    {
+        public string nome;
+        public int idade;
+    }
+}
